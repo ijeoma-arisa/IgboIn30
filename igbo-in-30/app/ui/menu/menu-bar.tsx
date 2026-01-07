446d1e5a2a35d0b4bb98ea8849cef70ca2b47024
Ijeoma Arisa <ijay.arisa@gmail.com>
2026-01-06 18:50:06 -0800

    Added inital menu bar and links for main menu

---
+++ igbo-in-30/app/ui/menu/menu-bar.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import MenuLinks from '@/app/ui/menu/menu-links';
+
+export default function MenuBar() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <Link
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-indigo-700 p-4 md:h-40"
+        href="/"
+      >
+      </Link>
+
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <MenuLinks />
+      <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+      </div>
+    </div>
+  );
+}
