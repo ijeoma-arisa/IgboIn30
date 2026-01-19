16355456b1738aa28d11f8a6c4d80c7729833e17
Ijeoma Arisa <ijay.arisa@gmail.com>
2026-01-18 17:04:44 -0800

    Finished Beginner and Intermediate Lessons playlists and updated names. Starting home page. Displayed today's date on homepage.

--- igbo-in-30/app/lib/data.ts
+++ igbo-in-30/app/lib/data.ts
@@ -68,15 +68,15 @@ export const instagramPages = [
 
 export const youtubePlaylists = [
   {
-    name: 'Igbo Learning Resources - @eejawma',
+    name: 'Igbo Learning Resources',
     embedText: '<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=Pm_1aTkjAKCeOjFi&amp;list=PLIcprcNO7gwEoev6mBMHrvrCDxtZL-jBH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
   },
   {
-    name: 'Beginner Igbo',
+    name: 'Beginner Igbo Lessons',
     embedText: '<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=Z1kwJeQsguPXAGqY&amp;list=PLIcprcNO7gwGDOc2woFJs-oLWf73EzcMc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
   },
   {
-    name: 'Intermediate Igbo',
+    name: 'Intermediate Igbo Lessons',
     embedText: '<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=rvH-wvbgLK7v9yb7&amp;list=PLIcprcNO7gwHTG70y4pf3O6sRO908OaXQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
   },
 ];
--- igbo-in-30/app/menu/(overview)/page.tsx
+++ igbo-in-30/app/menu/(overview)/page.tsx
@@ -1,9 +1,20 @@
 import { Metadata } from 'next';
+import Calendar from '@/app/ui/menu/calendar';
 
 export const metadata: Metadata = {
   title: 'Home',
 };
 
 export default function Page() {
-  return <p className="text-black">Home</p>
+  return (
+    <main>
+      <h1 className="text-black text-center mb-4 text-xl md:text-3xl font-bold">
+        {metadata.title?.toString()}
+      </h1>
+      <div className="flex flex-col gap-6 text-indigo-700 font-bold text-xl">
+        <p>Progress</p>
+        <Calendar />
+      </div>
+    </main>
+  );
 }
