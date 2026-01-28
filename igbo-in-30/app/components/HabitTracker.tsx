ff1d6df3fa4a0f110f1e73a4d76c67aaa14e5875
Ijeoma Arisa <ijay.arisa@gmail.com>
2026-01-28 14:08:36 -0800

    Added notes area and save button to Habit Tracker

--- igbo-in-30/app/components/HabitTracker.tsx
+++ igbo-in-30/app/components/HabitTracker.tsx
@@ -1,15 +1,12 @@
 'use client';
 
 import { studyOptions } from '@/app/ui/menu/menu-links';
-
 import clsx from 'clsx';
-
-
 
 const OPTIONS = ['YouTube Lessons']
 export default function HabitTracker(){
   return (
-    <section className="w-full max-w-2xl rounded-xl border border-gray-200 bg-white p-4 shadow-sm">
+    <section className="flex flex-col w-full max-w-2xl rounded-xl border border-gray-200 bg-white p-4 shadow-sm">
       <header className="mb-1 flex flex-col justify-center items-center">
         {/* Title */}
         <h2 className="text-lg font-semibold">
@@ -24,13 +21,26 @@ export default function HabitTracker(){
       </header>
 
        {/* Options */}
-       <div className="grid grid-cols-5 text-center">
+       <div className="grid grid-cols-5 text-center mb-4">
         { studyOptions.map((studyOption) => (
           <div key={studyOption}>
             {studyOption}
           </div>
         ))}
        </div>
+
+      {/* Notes */}
+       <textarea 
+          className="w-full h-full p-2 resize-none border rounded-md border-gray-500"
+          placeholder="Enter Notes here"
+       />
+
+      {/* Save button */}
+      <div className="mt-2 flex justify-end">
+        <button className="border rounded-lg px-8 py-1 font-semibold bg-purple-200 border-indigo-700 transition hover:bg-purple-400">
+          Save
+        </button>
+      </div>
     </section>
   );
 }
