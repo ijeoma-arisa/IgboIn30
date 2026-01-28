4dd28aca42d9b03c2731876ae7039564980eca38
Ijeoma Arisa <ijay.arisa@gmail.com>
2026-01-28 14:50:23 -0800

    Added temporary IgboIn30 logo to landing page. Changed background and orientation

--- igbo-in-30/app/page.tsx
+++ igbo-in-30/app/page.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-        <Image
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+    <div className="flex min-h-screen items-center justify-center bg-purple-100 font-sans">
+      <main className="flex min-h-full w-full max-w-3xl rounded-xl flex-col items-center justify-between py-32 px-16 bg-white">
+        {/* Temporary Logo */}
+        <div
+          className="flex justify-center mb-4 px-20 py-4 rounded-md bg-indigo-700 "
+        >
+          <p className="text-white text-3xl">IgboIn30</p>
+        </div>
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
             To get started, edit the page.tsx file.
