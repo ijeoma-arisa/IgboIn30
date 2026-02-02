4f8236d441f9b2c4834996bdb9fd533111614742
Ijeoma Arisa <ijay.arisa@gmail.com>
2026-02-01 22:37:30 -0800

    Used form and dropdown instead of checkboxes.

--- igbo-in-30/app/components/HabitTracker.tsx
+++ igbo-in-30/app/components/HabitTracker.tsx
@@ -1,20 +1,12 @@
 'use client';
 
-import { studyLinks } from '@/lib/data/menu-links-data';
+import { studyLinks, studyOptions } from '@/lib/data/menu-links-data';
 import { useState } from 'react';
 import clsx from 'clsx';
 
 export default function HabitTracker(){
   
-  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
-  
-  function toggleOption(option: string) {
-    setSelectedOptions((currentOptions) => 
-      currentOptions.includes(option)
-        ? currentOptions.filter((o) => o !== option )
-        : [...currentOptions, option]
-    );
-  }
+  const [selectedOption, setSelectedOption] = useState<typeof studyOptions[number]>(studyOptions[0]);
 
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
@@ -22,55 +14,52 @@ export default function HabitTracker(){
   }
   
   return (
-    <form onSubmit={handleSubmit}>
-      <section className="flex flex-col w-full max-w-2xl rounded-xl border border-gray-200 bg-white p-4 shadow-sm">
-        <header className="mb-1 flex flex-col justify-center items-center">
-          {/* Title */}
-          <h2 className="text-lg font-semibold">
-            Habit Tracker
-          </h2>
+    <form onSubmit={handleSubmit} className="flex flex-col w-full max-w-2xl rounded-xl border border-gray-200 bg-white p-4 shadow-sm">
+      <header className="mb-1 flex flex-col justify-center items-center">
+        {/* Title */}
+        <h2 className="text-lg font-semibold">
+          Habit Tracker
+        </h2>
+      </header>
 
+      {/* Options */}
+      <label>
           {/* Question */}
           <div className="mb-2 text-md text-center">
             <p> What did you learn today? </p>
             <p> (Gịnị ka ị mụtara taa?) </p>
           </div>
-        </header>
-
-        {/* Options */}
-        <div className="grid grid-cols-5 mb-4">
-          { studyLinks.map((studyLink) => {
-            const LinkIcon = studyLink.icon;
-            const studyOption = studyLink.name;
-
+        <select
+          value={selectedOption}
+          onChange={(e) => setSelectedOption(e.target.value as typeof selectedOption)}
+          className=""
+        >
+          {studyOptions.map((studyOption) => {
             return (
-              <div key={studyOption} className="flex items-center gap-2">
-                <input 
-                  key={studyOption}
-                  type="checkbox"
-                  checked={selectedOptions.includes(studyOption)}
-                  onChange={ () => toggleOption(studyOption)}
-                />
-                  <LinkIcon className="w-6" />
-                    { studyLink.name } 
-                </div>
-            );
+              <option 
+                key={studyOption}
+                value={studyOption}
+                className="flex gap-2"
+              >
+                {studyOption}
+              </option>
+              );
           })}
-        </div>
+        </select>
+      </label>
 
-        {/* Notes */}
-        <textarea 
-            className="w-full h-full p-2 resize-none border rounded-md border-gray-500"
-            placeholder="Enter Notes here"
-        />
+      {/* Notes */}
+      <textarea 
+          className="w-full h-full p-2 resize-none border rounded-md border-gray-500"
+          placeholder="Enter Notes here"
+      />
 
-        {/* Save button */}
-        <div className="mt-2 flex justify-end">
-          <button className="border rounded-lg px-8 py-1 font-semibold bg-purple-200 border-indigo-700 transition hover:bg-purple-400">
-            Save
-          </button>
-        </div>
-      </section>
+      {/* Save button */}
+      <div className="mt-2 flex justify-end">
+        <button className="border rounded-lg px-8 py-1 font-semibold bg-purple-200 border-indigo-700 transition hover:bg-purple-400">
+          Save
+        </button>
+      </div>
     </form>
   );
 }
