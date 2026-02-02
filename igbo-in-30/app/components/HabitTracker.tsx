a11fd05024e0fe943d1d9379de712abfb27c6f7d
Ijeoma Arisa <ijay.arisa@gmail.com>
2026-02-01 20:09:47 -0800

    Added checkboxes to habit tracker study options. Added hover fields to each menu link data object

--- igbo-in-30/app/components/HabitTracker.tsx
+++ igbo-in-30/app/components/HabitTracker.tsx
@@ -1,46 +1,76 @@
 'use client';
 
-import { studyOptions } from '@/lib/data/menu-links-data';
+import { studyLinks } from '@/lib/data/menu-links-data';
+import { useState } from 'react';
 import clsx from 'clsx';
 
-const OPTIONS = ['YouTube Lessons']
 export default function HabitTracker(){
+  
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+  
+  function toggleOption(option: string) {
+    setSelectedOptions((currentOptions) => 
+      currentOptions.includes(option)
+        ? currentOptions.filter((o) => o !== option )
+        : [...currentOptions, option]
+    );
+  }
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log("Submitted!");
+  }
+  
   return (
-    <section className="flex flex-col w-full max-w-2xl rounded-xl border border-gray-200 bg-white p-4 shadow-sm">
-      <header className="mb-1 flex flex-col justify-center items-center">
-        {/* Title */}
-        <h2 className="text-lg font-semibold">
-          Habit Tracker
-        </h2>
+    <form onSubmit={handleSubmit}>
+      <section className="flex flex-col w-full max-w-2xl rounded-xl border border-gray-200 bg-white p-4 shadow-sm">
+        <header className="mb-1 flex flex-col justify-center items-center">
+          {/* Title */}
+          <h2 className="text-lg font-semibold">
+            Habit Tracker
+          </h2>
 
-        {/* Question */}
-        <div className="mb-2 text-md text-center">
-          <p> What did you learn today? </p>
-          <p> (Gịnị ka ị mụtara taa?) </p>
-        </div>
-      </header>
-
-       {/* Options */}
-       <div className="grid grid-cols-5 text-center mb-4">
-        { studyOptions.map((studyOption) => (
-          <div key={studyOption}>
-            {studyOption}
+          {/* Question */}
+          <div className="mb-2 text-md text-center">
+            <p> What did you learn today? </p>
+            <p> (Gịnị ka ị mụtara taa?) </p>
           </div>
-        ))}
-       </div>
+        </header>
 
-      {/* Notes */}
-       <textarea 
-          className="w-full h-full p-2 resize-none border rounded-md border-gray-500"
-          placeholder="Enter Notes here"
-       />
+        {/* Options */}
+        <div className="grid grid-cols-5 mb-4">
+          { studyLinks.map((studyLink) => {
+            const LinkIcon = studyLink.icon;
+            const studyOption = studyLink.name;
 
-      {/* Save button */}
-      <div className="mt-2 flex justify-end">
-        <button className="border rounded-lg px-8 py-1 font-semibold bg-purple-200 border-indigo-700 transition hover:bg-purple-400">
-          Save
-        </button>
-      </div>
-    </section>
+            return (
+              <div key={studyOption} className="flex items-center gap-2">
+                <input 
+                  key={studyOption}
+                  type="checkbox"
+                  checked={selectedOptions.includes(studyOption)}
+                  onChange={ () => toggleOption(studyOption)}
+                />
+                  <LinkIcon className="w-6" />
+                    { studyLink.name } 
+                </div>
+            );
+          })}
+        </div>
+
+        {/* Notes */}
+        <textarea 
+            className="w-full h-full p-2 resize-none border rounded-md border-gray-500"
+            placeholder="Enter Notes here"
+        />
+
+        {/* Save button */}
+        <div className="mt-2 flex justify-end">
+          <button className="border rounded-lg px-8 py-1 font-semibold bg-purple-200 border-indigo-700 transition hover:bg-purple-400">
+            Save
+          </button>
+        </div>
+      </section>
+    </form>
   );
 }
--- igbo-in-30/lib/data/menu-links-data.ts
+++ igbo-in-30/lib/data/menu-links-data.ts
@@ -9,13 +9,13 @@ import {
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 
 export const links = [
-  { name: 'Home', href: '/menu', icon: HomeIcon, textColor: 'text-gray-500' },
-  { name: 'Good News', href: '/menu/good-news', icon: HeartIcon, textColor: 'text-pink-500'},
-  { name: 'YouTube Lessons', href: '/menu/youtube-lessons', icon: PlayCircleIcon, textColor: 'text-red-600'},
-  { name: 'Videos', href:'/menu/videos', icon: TvIcon, textColor:'text-blue-800' },
-  { name: 'Music', href:'/menu/music', icon: MusicalNoteIcon, textColor: 'text-purple-500' },
-  { name: 'Websites', href:'/menu/websites', icon: GlobeAltIcon, textColor: 'text-cyan-500' },
-  { name: 'Social Media', href: '/menu/social-media', icon: CameraIcon, textColor: 'text-amber-500' },
+  { name: 'Home', href: '/menu', icon: HomeIcon, textColor: 'text-gray-500', hover: 'hover:bg-gray-500' },
+  { name: 'Good News', href: '/menu/good-news', icon: HeartIcon, textColor: 'text-pink-500', hover: 'hover:bg-pink-500'},
+  { name: 'YouTube Lessons', href: '/menu/youtube-lessons', icon: PlayCircleIcon, textColor: 'text-red-600', hover: 'hover:bg-gray-500'},
+  { name: 'Videos', href:'/menu/videos', icon: TvIcon, textColor:'text-blue-800', hover: 'hover:bg-blue-800' },
+  { name: 'Music', href:'/menu/music', icon: MusicalNoteIcon, textColor: 'text-purple-500', hover: 'hover:bg-purple-500' },
+  { name: 'Websites', href:'/menu/websites', icon: GlobeAltIcon, textColor: 'text-cyan-500', hover: 'hover:bg-cyan-500' },
+  { name: 'Social Media', href: '/menu/social-media', icon: CameraIcon, textColor: 'text-amber-500', hover: 'hover:bg-amber-500' },
 ];
 
 export const studyLinks = links.slice(2);
